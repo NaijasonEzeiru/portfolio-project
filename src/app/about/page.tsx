c53b14c45abe66d7e6079829f41be4733f3bff21
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-07-07 02:02:55 +0100

    Added socials icons on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -58,14 +58,39 @@ const page = () => {
 				</p>
 			</div>
 			<div>
-				<h3 className='text-xl font-semibold'>
+				<h3 className='text-xl font-semibold mb-3'>
 					Creator&#39;s Details:
 				</h3>
-				<span>
-					<p>
-						LinkedIn: https://www.linkedin.com/in/chibuike-ezeiru/
-					</p>
-					<p>GitHub: https://github.com/NaijasonEzeiru</p>
+				<span className='flex flex-col gap-2'>
+					<span className='flex gap-2'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							fill='currentColor'>
+							<path d='M20.27,20.27H16.18V14.35c0-1.39-.03-3.17-1.93-3.17-1.92,0-2.22,1.5-2.22,3v5.09H8V9h3.75v1.59H12c.5-1,1.74-2.06,3.57-2.06,3.81,0,4.5,2.51,4.5,5.77v6.97H20.27ZM5.92,7.68C4.28,7.68,3,6.4,3,4.75S4.28,1.82,5.92,1.82,8.85,3.1,8.85,4.75,7.57,7.68,5.92,7.68ZM3.5,20.27h4.09V9.18H3.5Z' />
+						</svg>
+						<p>
+							LinkedIn:
+							https://www.linkedin.com/in/chibuike-ezeiru/
+						</p>
+					</span>
+					<span className='flex gap-2'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							fill='currentColor'>
+							<path
+								fillRule='evenodd'
+								clipRule='evenodd'
+								d='M12 0C5.37 0 0 5.37 0 12C0 17.3 3.438 21.57 8.205 23.313C8.805 23.428 9.01 23.058 9.01 22.74V20.706C6.564 21.38 5.913 19.714 5.913 19.714C5.51 18.787 4.84 18.466 4.84 18.466C3.688 17.84 4.928 17.85 4.928 17.85C6.084 17.896 6.624 19.083 6.624 19.083C7.776 20.633 9.502 20.087 10.035 19.813C10.152 18.706 10.522 18.036 10.912 17.726C7.972 17.426 4.858 15.995 4.858 11.388C4.858 10.106 5.292 9.066 6.052 8.266C5.927 8.017 5.468 6.824 6.177 5.149C6.177 5.149 7.137 4.858 9.01 6.196C9.963 5.97 11.037 5.97 11.991 6.196C13.864 4.858 14.824 5.149 14.824 5.149C15.533 6.824 15.073 8.017 14.948 8.266C15.708 9.066 16.142 10.106 16.142 11.388C16.142 15.995 13.027 17.426 10.087 17.726C10.491 18.036 10.867 18.706 10.867 19.813V22.73C10.867 23.045 11.07 23.428 11.685 23.313C16.568 21.566 20 17.288 20 12C20 5.37 14.63 0 8 0Z'
+							/>
+						</svg>
+						<p>GitHub: https://github.com/NaijasonEzeiru</p>
+					</span>
 				</span>
 			</div>
 		</div>
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -51,8 +51,9 @@ const NavBar = () => {
 				className={`flex gap-1 w-full left-0 absolute transition-transform z-50 bg-gray-100 flex-col py-8 px-2 top-11 md:w-auto md:static md:bg-transparent md:flex-row md:translate-x-0 md:py-0 dark:bg-secondary ${
 					!ham && 'transition-transform -translate-x-full'
 				}`}>
-							d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
-				<Link href="/about"><p className='table px-3 w-full cursor-pointer'>Login</p></Link>
+				<Link href='/about'>
+					<p className='table px-3 w-full cursor-pointer'>About</p>
+				</Link>
 			</nav>
 
 			<div className='flex gap-3 items-center'>
