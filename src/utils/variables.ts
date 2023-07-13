a5d38955fd0ff4ceb9e24e24a1fa2a7354dfb5a0
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-07-13 04:15:38 +0100

    changed backend api

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,2 +1,3 @@
 // export const apiAddress = 'http://localhost:4000';
-export const apiAddress = 'https://test-wuik.onrender.com';
+// export const apiAddress = 'https://test-wuik.onrender.com';
+export const apiAddress = 'https://test1-cj25.onrender.com';
